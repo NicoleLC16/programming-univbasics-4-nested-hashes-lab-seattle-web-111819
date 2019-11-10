2c31393fccab0f417210a252512d370aa3069310
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-10 17:59:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -61,11 +61,6 @@ programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
 
 	programmer_hash = 
  		{
@@ -87,6 +82,8 @@ programmer_hash.merge(:yukihiro_matsumoto => {
    :known_for => "Ruby",
     :languages => ["LISP, C"]
  })
+ 
+ 
 end
 
 def changing_alan
