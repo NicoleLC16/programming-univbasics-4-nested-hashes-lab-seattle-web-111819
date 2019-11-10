0dd223ab4f6c8cdc373c16d60c046ebad1885c8b
Nicole Cabral <asalwaysnicole@outlook.com>
2019-11-10 17:49:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,13 +83,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash.merge ( 
-  :yukihiro_matsumoto => {
-    :known_for => "Ruby",
-    :languages => ["LISP", "C"]
-  } 
-  )
-    
+programmer_hash.merge(:yukihiro_matsumoto => {
+   :known_for => "Ruby",
+    :languages => ["LISP, C"]
+ })
 end
 
 def changing_alan
